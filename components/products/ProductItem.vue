70367acd4dd78a26fd6b3f62153b2eeacddcc516
Cristian Benitez <cristian.benitez@peiky.com>
2020-03-18 22:31:39 -0500

    add products components

---
+++ components/products/ProductItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img :src="product.photo_url" :alt="product.name">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/products/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(product, index) in products" :key="index">
+      <ProductItem :product="product" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ProductItem from '@/components/products/ProductItem.vue'
+
+export default {
+  name: 'ProductList',
+  components: {
+    ProductItem
+  },
+  computed: mapGetters({
+    products: 'products/products'
+  })
+}
+</script>
